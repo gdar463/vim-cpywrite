f01c02bc1cde7975c93a4a7a9f340f523f577578
gdar463 <dev@gdar463.com>
2026-08-29 21:56:16 +0200

    fix: existing new lines check started from wrong line
    Signed-off-by: gdar463 <dev@gdar463.com>

--- plugin/cpywrite/main.py
+++ plugin/cpywrite/main.py
@@ -132,23 +132,25 @@ def _write_header(writer, curr_buffer, filetype, filename):
 
             offset = max(0, offset - extra_new_lines + to_skip)
 
-            new_lines_offset = offset
-            before_extra_new_lines = extra_new_lines
-            has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
-            while before_extra_new_lines > 0 and has_newline and new_lines_offset > 0:
-                before_extra_new_lines -= 1
-                new_lines_offset -= 1
-                if new_lines_offset > 0:
-                    has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
+            new_lines_offset = offset - 1
+            if new_lines_offset > 0:
+                before_extra_new_lines = extra_new_lines
+                has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
+                while before_extra_new_lines > 0 and has_newline and new_lines_offset > 0:
+                    before_extra_new_lines -= 1
+                    new_lines_offset -= 1
+                    if new_lines_offset > 0:
+                        has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
 
-            new_lines_offset = offset
-            after_extra_new_lines = extra_new_lines
-            has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
-            while before_extra_new_lines > 0 and has_newline and new_lines_offset < len(curr_buffer):
-                after_extra_new_lines -= 1
-                new_lines_offset += 1
-                if new_lines_offset < len(curr_buffer):
-                    has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
+            new_lines_offset = offset + 1
+            if new_lines_offset < len(curr_buffer):
+                after_extra_new_lines = extra_new_lines
+                has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
+                while after_extra_new_lines > 0 and has_newline and new_lines_offset < len(curr_buffer):
+                    after_extra_new_lines -= 1
+                    new_lines_offset += 1
+                    if new_lines_offset < len(curr_buffer):
+                        has_newline = curr_buffer[new_lines_offset] == "" or curr_buffer[new_lines_offset] == "\n"
             curr_buffer[offset:offset] = ([""] * before_extra_new_lines if offset != 0 else []) + header.splitlines() + [""] * after_extra_new_lines
 
     except (ValueError, vim.error) as exc:
